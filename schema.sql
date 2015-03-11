6a2ad2d67f15bd37304a9f9eeff5f93d407ba38d
nookieaaa <nookieaaa@hotmail.com>
2015-03-11 15:29:06 +0200

    schema

--- schema.sql
+++ schema.sql
@@ -10,19 +10,19 @@ CREATE TABLE if not exists users (
   UNIQUE (email)
 );
 
-CREATE TABLE not exists authors (
+CREATE TABLE if not exists authors (
   id INTEGER NOT NULL,
   name VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE not exists books (
+CREATE TABLE if not exists books (
   id INTEGER NOT NULL,
   title VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE not exists books_authors (
+CREATE TABLE if not exists books_authors (
   fk_book INTEGER,
   fk_author INTEGER,
   FOREIGN KEY(fk_book) REFERENCES books (id),
